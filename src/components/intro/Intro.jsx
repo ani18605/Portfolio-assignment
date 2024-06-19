572e49d80d95ab2cfebb471221405115daa77884
ani18605 <152686668+ani18605@users.noreply.github.com>
2024-06-19 11:26:57 +0530

    Update Intro.jsx

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { FaAward } from 'react-icons/fa';
 import { VscFolderLibrary } from 'react-icons/vsc';
 import ME from '../../assets/me.jpeg';
+import Typical from 'react-typical';
 import './intro.css';
 
 const Intro = () => {
@@ -23,12 +24,29 @@ const Intro = () => {
               <small>20+ Completed Projects</small>
             </article>
           </div>
-          <p>a first-year student at Scaler School of Tech with a passion for competitive programming and web development. I enjoy solving algorithmic challenges on platforms like Codeforces and LeetCode, and I have experience building dynamic web applications using HTML, CSS, JavaScript, Node.js, and Spring Boot. My projects range from full-stack web apps to algorithm visualizers, reflecting my commitment to continuous learning and innovation. I'm excited to collaborate with the tech community and look forward to making a meaningful impact in the world of technology.</p>
+          <p>
+            <Typical
+              loop={Infinity}
+              wrapper="b"
+              steps={[
+                'a first-year student at Scaler School of Tech with a passion for competitive programming and web development. ',
+                1000,
+                'I enjoy solving algorithmic challenges on platforms like Codeforces and LeetCode, ',
+                1000,
+                'and I have experience building dynamic web applications using HTML, CSS, JavaScript, Node.js, and Spring Boot. ',
+                1000,
+                'My projects range from full-stack web apps to algorithm visualizers, reflecting my commitment to continuous learning and innovation. ',
+                1000,
+                'I\'m excited to collaborate with the tech community and look forward to making a meaningful impact in the world of technology.',
+                1000,
+              ]}
+            />
+          </p>
           <a href="#contact" className="btn btn-primary">Let's Talk</a>
         </div>
       </div>
     </section>
-  )
+  );
 }
 
-export default Intro
+export default Intro;
